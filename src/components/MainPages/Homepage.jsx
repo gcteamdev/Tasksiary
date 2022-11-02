bf4bd623766b3a9c0255f03cabe80e9926df5878
GC <contact@gcteam.dev>
2022-11-02 13:58:24 -0700

    implemented sidebar, created static pages and sign in page

--- src/components/MainPages/Homepage.jsx
+++ src/components/MainPages/Homepage.jsx
@@ -9,7 +9,7 @@ function Homepage() {
       <h1> Welcome! to Homepage</h1>
       <h6> Header</h6>
       </div>
-      
+      {/* Below are the componenets of Homepage flexbox is undone, need more work! */}
       <div className="containers">
         <div className="container1">
           <div className="projects-snap">
@@ -17,25 +17,26 @@ function Homepage() {
           </div>
           <div className="dashboard-snap">
           <h3>Dashboard</h3>  
-          </div>
+          </div> 
         </div>
-        <div classNameName="container2">
-          <div className="team-snap">
+        <div classNameName="container2 col-3">
+           <div className="team-snap">
           <h3>Team Members</h3> 
           </div>
           <div className="myTasks-snap">
           <h3>My Tasks</h3> 
-          </div>
+          </div> 
         </div>
-        <div classNameName="container3">
+        <div classNameName="container3 col-3">
           <div className="calender-snap">
-          <h3>Calender</h3> 
+           <h3>Calender</h3> 
           </div>
           <div className="inbox-snap">
-          <h3>Inbox</h3> 
+           <h3>Inbox</h3>  
+           <h5>Messages from Team members</h5>
           </div>
           <div className="chat-snap">
-          <h3>Chat</h3> 
+           <h3>Chat</h3>  
           </div>
         </div>
        
