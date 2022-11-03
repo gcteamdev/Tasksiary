65b99ebf019381ac5981cae4cdfbed748d327ea8
GC <contact@gcteam.dev>
2022-11-03 11:43:16 -0700

    corrected hr line in sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import Login from './components/Screens/Login';
 import Auth from './components/Auth';
+import "@fontsource/barlow";
 
 
 function App() {
